8959ec1bd992db83d61e12104edb2cdfa145407e
Masquerade786 <spasm6969786@gmail.com>
2023-10-19 11:14:46 +0530

    🚀: Added Morgan for logging

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const routes = require('./routes');
 const db = require('./db'); 
 const app = express();
 require('dotenv').config()
+const morgan = require('morgan');
     
 
 app.use(express.json());
+app.use(morgan('combined'))
 app.use('/api',routes);
 
 app.listen(3000,()=>{
